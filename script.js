884e9c8b5513eb0b16249987ef5bd0f26e1a9e3d
dilshod_akramov <121122743+DilshodAkramov@users.noreply.github.com>
2023-05-22 11:05:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let body = document.querySelector("body");
 let container = document.querySelector(".container");
 let songName = document.querySelectorAll(".song-name-music");
 let img = document.querySelector(".photo img");
+let photoContainer = document.querySelector(".photo");
 let currentMinute= document.querySelector(".current-minute");
 let totalMinute= document.querySelector(".total-minute");
 let progressRange = document.querySelector(".range");
@@ -11,17 +12,62 @@ let playCircle = document.querySelector(".play-circle");
 let nextBtn = document.querySelector(".next-btn");
 let playlist = document.querySelector(".playlist");
 let musicList = document.querySelector(".music-lists");
+let musicListUl = document.querySelector(".music-lists ul");
 let closeIcon = document.querySelector(".close-icon");
 let volume = document.querySelector(".volume-div input");
 let expandMore = document.querySelector(".expand-more");
 let audio = document.querySelector(".haupt-audio");
-let switchBtn = document.querySelector(".switch");
+let switchBtn = document.querySelector("#switch");
+let change = document.querySelector(".switched-on");
+let subInfo = document.querySelector(".sub-info");
+let addNewMusicBtn = document.querySelector(".expand-actions .add");
+
+let addingWindow = document.querySelector(".adding-music-window");
+let closingAddingWindow = document.querySelector(".close-adding-window");
+let musicTitle = document.querySelector(".music-title");
+let musicImg = document.querySelector(".music-img");
+let musicSrc = document.querySelector(".music-src");
+let submitBtn = document.querySelector(".submit-new-music");
+
+
+// window.addEventListener('load', ()=> {
+//     songs.push(localStorage.getItem("newSong"));
+// });
+
+
+
+
+
+
+closingAddingWindow.addEventListener("click", () => {
+    addingWindow.style.display = "none";
+})
+
+
+addNewMusicBtn.addEventListener("click", () => {
+    addingWindow.style.display = "block";
+
+})
+
+
+
+
+expandMore.addEventListener("click", () => {
+    container.classList.toggle("example1");
+    photoContainer.classList.toggle("photo-example");
+    subInfo.classList.toggle("example-sub-info");
+    // if(container.classList.contains("example1")){
+    //     photoContainer.style.display = "none";
+    // }else{
+    //     photoContainer.style.display = "block";
+        
+    // }
+})
 
 
 switchBtn.addEventListener("click", () =>{
-
-    body.style.backgroundColor = "#212429";
-    container.style.backgroundColor = "#1e2126";
+    body.classList.toggle("darkmode");
+    change.classList.toggle("toggle1");
 })
 
 
@@ -30,7 +76,7 @@ let seconds;
 let currentTime;
 let musicDuration;
 let progressWidth;
-
+let songs =[];
 
 
 
@@ -46,7 +92,31 @@ closeIcon.addEventListener("click", e => {
     musicList.style.height = 0;
 })
 
-let songs = [
+
+
+
+
+
+
+
+
+
+songs = [
+    // {
+    //     title: "Death Grips - Beware",
+    //     image: "images/photo.jpg",
+    //     music: "musics/music-1.mp3",
+    // },
+    // {
+    //     title: "Death Grips - Blackjack",
+    //     image: "images/photo-1.jpg",
+    //     music: "musics/music-2.mp3"
+    // },
+    // {
+    //     title: "Death Grips - Get Got",
+    //     image: "images/photo-2.jpg",
+    //     music: "musics/music-3.mp3"
+    // },
     {
         title: "Death Grips - Beware",
         image: "images/photo.jpg",
@@ -57,17 +127,62 @@ let songs = [
         image: "images/photo-1.jpg",
         music: "musics/music-2.mp3"
     },
-    {
-        title: "Death Grips - Get Got",
-        image: "images/photo-2.jpg",
-        music: "musics/music-3.mp3"
-    },
+    // {
+    //     title: "Death Grips - Get Got",
+    //     image: "images/photo-2.jpg",
+    //     music: "musics/music-3.mp3"
+    // },
     {
         title: "Death Grips - I Want It I Need It",
         image: "images/photo-3.jpg",
         music: "musics/music-4.mp3"
     }
 ];
+
+
+
+
+// Adding New Song 
+
+let newSongAdded;
+
+submitBtn.addEventListener("click", (e) =>{
+
+    e.preventDefault();
+    newSongAdded = {
+        title: document.querySelector(".music-title").value,
+        image: musicImg.value,
+        music: musicSrc.value
+    }
+
+    if(submitBtn){
+        addingWindow.style.display = "none";
+    }
+
+    
+    songs.push(newSongAdded);
+    
+    
+    
+    localStorage.setItem("newSong", JSON.stringify(songs));
+    
+    // console.log(localStorage);
+    // console.log(songs);
+    
+    
+})
+
+// console.log(localStorage);
+
+
+function getAllSongs(){
+    songs.forEach((song) => {
+        console.log(song);
+    })
+};
+
+getAllSongs();
+
 
 
 let counter = 0;
@@ -128,17 +243,53 @@ function loadMusic(){
 };
 
 
+
+creatingLi();
+
+
+function creatingLi(e){
+
+    let local = localStorage.getItem("newSong");
+
+    let obj = JSON.parse(local);
+    for (let i = 0; i < songs.length; i++){
+    
+        let li = document.createElement("li");
+        
+        li.innerHTML = `<div>       
+                            <span>${songs[i].title}</span>
+                            <span>New Playlist</span>
+                        </div>
+                        <div class="time">
+                            <span>${"loadMusic()"}</span>
+                        </div>`;
+        
+        li.classList.add("li");
+    
+        musicListUl.appendChild(li);
+    }
+}
+
+
+
+
 // nextBtn 
 
 nextBtn.addEventListener("click", nextSong);
 
 function nextSong(){
+    
     counter++;
     if(counter > songs.length - 1){
         counter = 0;
     }
     loadMusic(songs[counter]);
     audio.play();
+    if(nextBtn){
+        playCircle.innerHTML = `<span class="play-circle material-symbols-outlined">
+        pause_circle</span>`;
+    }
+    
 }
 
 
@@ -154,8 +305,14 @@ function prevSong(){
     loadMusic(songs[counter]);
     audio.play();
     
+    if(nextBtn){
+        playCircle.innerHTML = `<span class="play-circle material-symbols-outlined">
+        pause_circle</span>`;
+    }
     
 }
+
+
 
 
 // Play and Pause Music/ 
@@ -171,15 +328,32 @@ function pauseMusic(){
     container.classList.remove("paused");
     playCircle.innerHTML = `
     <span class="play-circle material-symbols-outlined">play_circle</span>`;
+    
     audio.pause();
 }
+
+
 
 
 // playCircle 
 
 playCircle.addEventListener("click", () =>{
     let isMusicPlaying = container.classList.contains("paused");
-
+    
+    
     isMusicPlaying ? pauseMusic() : playMusic();
+    
+    
+    
+    if(pauseMusic){
+        photoContainer.classList.remove("photo-running");
+    }else{
+        photoContainer.classList.add("photo-running");
+    }
+    // if(pauseMusic){
+        //     photoContainer.style.animationPlayState = "paused";
+        // }
+        
+
 });
 
